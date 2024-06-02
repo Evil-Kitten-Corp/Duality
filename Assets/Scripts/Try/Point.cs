4cfb18cb8318100552a0f047b8452312d60f8762
arbyun <115217686+arbyun@users.noreply.github.com>
2024-06-02 19:41:19 +0100

    add collectible objects

---
+++ Assets/Scripts/Try/Point.cs
@@ -0,0 +1,30 @@
+﻿using Test;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Try
+{
+    public class Point : NetworkBehaviour
+    {
+        public FruitType team;
+        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!col.CompareTag("Player")) return;
+            if (!NetworkManager.Singleton.IsServer) return;
+
+            if (col.TryGetComponent(out PlayerScore ps) && ps.team == team)
+            {
+                FindObjectOfType<SpawnRealPlayer>().AddScore(team);
+                //ps.AddScore();
+                NetworkObject.Despawn();
+            }
+            else if (col.TryGetComponent(out PlayerMove pm) && pm.fruit == team)
+            {
+                FindObjectOfType<SpawnRealPlayer>().AddScore(team);
+                //pm.GetComponent<PlayerScore>().AddScore();
+                NetworkObject.Despawn();
+            }
+        }
+    }
+}
