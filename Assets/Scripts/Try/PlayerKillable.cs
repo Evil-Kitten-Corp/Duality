321b7b2ac101acaf20f91fbe4b2f40b0a79cd86b
arbyun <115217686+arbyun@users.noreply.github.com>
2024-06-02 19:40:26 +0100

    add enemy objects

---
+++ Assets/Scripts/Try/PlayerKillable.cs
@@ -0,0 +1,48 @@
+﻿using Test;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Try
+{
+    public class PlayerKillable : NetworkBehaviour
+    {
+        public bool killsEveryone;
+        public FruitType team;
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.TryGetComponent(out PlayerMove pm))
+            {
+                if (killsEveryone || pm.fruit != team)
+                {
+                    if (NetworkManager.Singleton.IsServer)
+                    {
+                        HandleCollision(pm);
+                    }
+                    else
+                    {
+                        NotifyServerOfCollisionServerRpc(pm.NetworkObjectId);
+                    }
+                }
+            }
+        }
+
+        private void HandleCollision(PlayerMove pm)
+        {
+            pm.Die();
+            FindObjectOfType<SpawnRealPlayer>().OnPlayerDeath();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void NotifyServerOfCollisionServerRpc(ulong playerNetworkObjectId)
+        {
+            if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(playerNetworkObjectId, out var playerObject))
+            {
+                if (playerObject.TryGetComponent(out PlayerMove pm))
+                {
+                    HandleCollision(pm);
+                }
+            }
+        }
+    }
+}
